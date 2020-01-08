ede11aede53a007158d365f1c8c1627938d8470c
Gytis <velniopviska@gmail.com>
2020-01-08 11:32:02 +0200

    rooted

---
+++ bootloader.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+require 'config.php';
+
+// Load Core Classes
+//require ROOT . '/vendor/autoload.php';
+
+// Load Core Functions
+require ROOT . '/core/functions/form/core.php';
+require ROOT . '/core/functions/html/builder.php';
+
+// Load App Functions
+require ROOT . '/app/functions/validators.php';
---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../bootloader.php';
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="media/css/normalize.css">
+        <link rel="stylesheet" href="media/css/milligram.min.css">
+        <link rel="stylesheet" href="media/css/style.css">		
+        <title>OOP</title>
+    </head>
+    <body>
+		<h1>Darome HIP, darome OOP bl</h1>
+    </body>
+</html>
